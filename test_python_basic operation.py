d2bfd4cd2480c2bf3bcea747c6b4bd3a16e102a0
forspy <40792536+forspy@users.noreply.github.com>
2018-11-21 20:32:10 +0800

    测试_py_基础_print_import_#!_turtle

--- test_python_basic operation.py
+++ test_python_basic operation.py
@@ -4,6 +4,9 @@ print(1 // 2)  # 这样是向下取整的除法
 print(10 % 3)  # python求mod
 # 多个字符串的连接输出
 print("hello" + "world!" + '\n' + '!')  # 1.python的字符串连接方式 2.python识别\n
+print("hello\nworld!")
+print("c:\\test.txt")
+print(r"c:\test.txt")
 # reformat Code是自动对齐 快捷键：Ctrl+Alt+L
 # 右键.py窗口，选择Show in explorer，可打开文件所在位置
 # Ctrl+z 撤销
@@ -58,6 +61,7 @@ print((1 + 1j) * (1 - 1j))  # 复数运算
 from turtle import *
 
 forward(100)  # 红线是pycharm的一个小bug
+left(120)  # 转120度
 forward(100)
 left(120)
 forward(100)
@@ -85,4 +89,19 @@ mainloop()  # 这句话可以保持窗口
 
 # 方法2、如开发过程中无需R，直接卸载R语言插件
 # Files->Settings->Plugins->R Language Support->Uninstall
+s = str(111)#将int转化成字符串
+print(s)
 
+#!/usr/bin/env python3
+'''
+大部分python文件的头部都会写上 #!/usr/bin/python 或者 #!/usr/bin/env ，这个语句主要和运行模式有关，
+如果我们用普通运行模式例如(linux) ： python *.py 那么这个语句在此运行模式下无效。如果想让python程序像普通程序一样运行，
+例如：./*.py (文件要有可执行权限chmod a+x *.py，然后执行./*.py)，这个语句就起作用了，他用来为脚本语言指定解释器，
+通常认为用 #!/usr/bin/env python 要比 #!/usr/bin/python 更好，因为 python 解释器有时并不安装在默认路径，例如在虚拟环境中。
+第一：#!/usr/bin/env python
+这种写法在你机器上安装了多个版本的python的时候有意义，这样声明的时候，会去取你机器的 PATH 中指定的第一个 python 来执行你的脚本。
+如果这时候你又配置了虚拟环境的话，那么这样写可以保证脚本会使用你虚拟环境中的 python 来执行。
+第二：#!/usr/bin/python
+表示写死了就是要 /usr/bin/python 这个目录下 python 来执行你的脚本。这样写程序的可移植性就差了，如果此路径下python命令不存在就会报错。
+所以一般情况还是用第一种写法。
+'''
