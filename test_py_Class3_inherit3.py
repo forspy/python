02618d80cf5c3c23353f343fe2648bf6af26c10b
forspy <qiubow@126.com>
2019-03-11 14:31:42 +0800

    测试_py_访问序列计数器_继承list方法_propertyI/O分配

--- test_py_Class3_inherit3.py
+++ test_py_Class3_inherit3.py
@@ -14,5 +14,50 @@ print(c1)
 c1.reverse()
 del c1[0]
 print(c1)
-print(c1[0]+c1[1])
-print('访问序列次数',c1.counter)
+print(c1[0] + c1[1])
+print('访问序列次数', c1.counter)
+
+
+# property()智能给成员变量分配值以及读取的方法
+# 先看原始方法
+class Rectangel:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+
+    def set_size(self, size):
+        self.width, self.height = size  # 这样的赋值方式使得size成为了元组
+
+    def get_size(self):
+        return self.width, self.height  # python能有多个返回值，牛逼
+
+
+r1 = Rectangel()
+r1.width = 10
+r1.height = 5
+print(r1.get_size())
+r1.set_size((100, 150))
+print(r1.width, r1.height)
+
+
+# 使用property后
+class Rectange:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+
+    def set_size(self, size):
+        self.width, self.height = size  # 这样的赋值方式使得size成为了元组
+
+    def get_size(self):
+        return self.width, self.height  # python能有多个返回值，牛逼
+
+    size = property(get_size, set_size)  # 只需要将get_size和set_size作为参数关联到property里面，然后重新命名一下就能使用property智能判别I/O方式
+    # 条件：新式类 在老式python编译器里面使用__metaclass__
+    # 深层：property是一个类，实现了一些__get__,__set__,__delete__方法，定义了描述符协议，将这些协议关联到寄生类的相关方法上
+
+
+r = Rectange()
+print(r.size)
+r.size = 150, 100
+print(r.width, r.height)
