5e5a8d6faf2ff57d4fb1207027e855e26bdf6d13
forspy <qiubow@126.com>
2019-03-18 11:12:02 +0800

    项目_py_文本编辑器_GUI_TKINTER_事件_控件_布局_bind事件绑定

--- test_py_GUI_Tkinter.py
+++ test_py_GUI_Tkinter.py
@@ -1,12 +1,12 @@
 # 要使用python的GUI必须导入tkinter模块，使用GUI需要安装GUI工具包
 # 实现一个简单的文本打开编辑器
-#使用方法，先save 创建一个.txt，然后open
+# 使用方法，先save 创建一个.txt，然后open
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText  # 多行可滚动文本区域
 
 
 def load():
-    with open(filename.get()) as file:#使用with来自动打开和关闭一个文件
+    with open(filename.get()) as file:  # 使用with来自动打开和关闭一个文件
         contents.delete('1.0', END)
         contents.insert(INSERT, file.read())
 
@@ -27,4 +27,13 @@ filename.pack(side=LEFT, expand=True, fill=X)
 Button(text='Open', command=load).pack(side=LEFT)  # 使用回调函数
 Button(text='Save', command=save).pack(side=LEFT)
 
-mainloop()
+
+# 要添加事件处理程序，可使用方法bind
+# 以下为测试方法，将鼠标点击事件和输出函数绑定
+def callback(event):
+    print(event.x, event.y)
+
+
+print(top.bind('<Button-1>', callback))
+##############
+mainloop()  # 主事件循环 阻塞在这儿
